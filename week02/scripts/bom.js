3f557d6cbe7018f4d97eb2e072b0c16c44f629cc
Joaquin Morales <joaquin.moralestr@gmail.com>
2024-06-10 23:46:54 -0400

    Add events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,25 @@ const input = document.querySelector('#favchap')
 const button = document.querySelector('button')
 const list = document.querySelector('#list')
 
-const listItem = document.createElement('li')
-const deleteButton = document.createElement('button')
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const listItem = document.createElement('li')
+        const deleteButton = document.createElement('button')
 
-listItem.textContent = input.value
-deleteButton.textContent = '❌'
+        listItem.textContent = input.value
+        deleteButton.textContent = '❌'
 
-listItem.append(deleteButton)
-list.append(listItem)
+        listItem.append(deleteButton)
+        list.append(listItem)
+
+        deleteButton.addEventListener('click', function() {
+            listItem.remove()
+            input.focus()
+        })
+
+        input.value = ''
+        input.focus()
+    } else {
+        input.focus()
+    }
+})
